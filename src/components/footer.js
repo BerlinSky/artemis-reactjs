cb273259b5ccde76a790055ae7e31cdca70a6633
Berlin Sky <pedroyoung8989@gmail.com>
2016-10-19 21:58:36 +0200

    Clean up

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppFooter = () => {
 	return (
 	  <footer className="l-footer footer">
-	    <div className="copyright">&copy; Inspired by Athena</div>
+	    <div className="copyright">&copy; Inspired by Artemis</div>
 	  </footer>
 	)
 }
